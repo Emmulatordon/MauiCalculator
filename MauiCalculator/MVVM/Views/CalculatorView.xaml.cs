8905573cb4ac7e8f0c23c3e9d721f12bcc7a1de5
viva <emmulatordon@gmail.com>
2025-03-10 18:15:39 +0100

    finished maui calculator

--- MauiCalculator/MVVM/Views/CalculatorView.xaml.cs
+++ MauiCalculator/MVVM/Views/CalculatorView.xaml.cs
@@ -1,3 +1,5 @@
+using MauiCalculator.MVVM.ViewModels;
+
 namespace MauiCalculator.MVVM.Views;
 
 public partial class CalculatorView : ContentPage
@@ -5,5 +7,6 @@ public partial class CalculatorView : ContentPage
 	public CalculatorView()
 	{
 		InitializeComponent();
+		BindingContext = new CalcViewModel();
 	}
 }
